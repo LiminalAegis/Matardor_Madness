19e2134a205dab02d42d01ec76d83b37a1e1e885
Isabel Gutierrez <iguti@live.com>
2025-03-31 16:18:10 -0400

    Bull work

--- Assets/Scripts/Bull.cs
+++ Assets/Scripts/Bull.cs
@@ -1,18 +1,69 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using NETWORK_ENGINE;
 
-public class Bull : MonoBehaviour
+public class Bull : NetworkComponent
 {
+    public NavMeshAgent agent;
+    public float distance;
+    
+
+    public override void HandleMessage(string flag, string value)
+    {
+        
+    }
+
+    public override void NetworkedStart()
+    {
+        
+    }
+
+    public override IEnumerator SlowUpdate()
+    {
+        while (IsConnected)
+        {
+
+
+
+            yield return new WaitForSeconds(MyCore.MasterTimer);
+        }
+    }
+
+    public Vector3 Roam()
+    {
+        Vector3 pickDir = UnityEngine.Random.insideUnitSphere * distance; //get position in circle
+        
+        pickDir += this.transform.position; //add direction to our position
+
+        NavMeshHit hit;
+
+        NavMesh.SamplePosition(pickDir, out hit, distance, -1);
+
+        return hit.position;
+    }
+
+    public void Rush()
+    {
+
+    }
+
+    public void Rest()
+    {
+
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+        Roam();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
 }
