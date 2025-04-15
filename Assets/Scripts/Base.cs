4ef04df727f161e2c169418c72c350e73c862789
Isabel Gutierrez <158535623+izzyjguti@users.noreply.github.com>
2025-04-14 20:19:52 -0400

    Edits

---
+++ Assets/Scripts/Base.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NETWORK_ENGINE;
+
+public class Base : NetworkComponent
+{
+    Material pink, green, blue, orange;
+    MeshRenderer baseFloor;
+
+    public override void HandleMessage(string flag, string value)
+    {
+        if (flag == "COLOR")
+        {
+            ColorSet(int.Parse(value));
+        }
+    }
+
+    public override void NetworkedStart()
+    {
+        
+    }
+
+    public override IEnumerator SlowUpdate()
+    {
+        yield return null;
+    }
+
+    public void ColorSet(int color)
+    {
+        switch (color)
+        {
+            case 0:
+                baseFloor.material = pink;
+                break;
+            case 1:
+                baseFloor.material = green;
+                break;
+            case 2:
+                baseFloor.material = blue;
+                break;
+            case 3:
+                baseFloor.material = orange;
+                break;
+
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
